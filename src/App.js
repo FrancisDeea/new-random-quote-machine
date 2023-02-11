d3efa3b3e88a6cf3c01b69ca2c5a67c4bbc7d702
Francis <francisbernal14@gmail.com>
2023-02-12 00:29:07 +0100

    feat: add method to fetch random quotes in GetData component

--- src/App.js
+++ src/App.js
@@ -10,11 +10,25 @@ class GetData extends React.Component {
     }
   }
 
+  componentDidMount() {
+    fetch("https://api.quotable.io/random")
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        };
+        return response.json();
+      })
+      .then(data => {
+        this.setState({author: data.author, text: data.content})
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <figure className="data-container">
-        <blockquote id="text"></blockquote>
-        <figcaption id="author"></figcaption>
+        <blockquote id="text">{this.state.text}</blockquote>
+        <figcaption id="author">{this.state.author}</figcaption>
       </figure>
     )
   }
